49cb25c53e811c4def5bfc4ee622405fe91cec8b
Yanet Villa Rojo <yanetrojo01@gmail.com>
2022-04-28 19:08:49 -0500

    Practica 12

--- Practica12/script.js
+++ Practica12/script.js
@@ -99,7 +99,7 @@ function iniciaMapa() {
     
     // MAPA 6
 
-    var mapa6 = new google.maps.Map(document.getElementById('mapa6'), propiedades6)
+    var mapa6 = new google.maps.Map(document.getElementById('mapa6'), propiedades)
     
     google.maps.event.addDomListener(mapa6, 'click', function() {
         window.alert(' Se hizo click en el mapa');
